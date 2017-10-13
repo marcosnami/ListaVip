ad37c6db1c611caa0b9fd6031b7fe94b215103e1
MarcosNami <marcosnami@hotmail.com>
2017-10-13 08:22:21 -0300

    First Commit

---
+++ src/main/java/com/alura/listavip/ListaVipApplication.java
@@ -0,0 +1,15 @@
+package com.alura.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by MarcosNami on 10/12/2017.
+ */
+@SpringBootApplication
+public class ListaVipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaVipApplication.class, args);
+    }
+}
